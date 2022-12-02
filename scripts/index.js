4daa8d9f4321f05ee8b35f8cd17408db062fab40
Mariya Ilina <mariy4ilina@yandex.ru>
2022-12-02 15:59:59 +0400

    feature/js

--- scripts/index.js
+++ scripts/index.js
@@ -20,6 +20,18 @@ function closePopup() {
 
 }
 
+// const openPopup =(popup) => {
+//   popup.classList.add('popup_is-opened')
+// }
+// const closePopup= ()=> {
+//   popupElement.forEach(function (element) {
+//     element.classList.remove('popup_is-opened')
+//   })
+// }
+// const editProfile = ( => {
+//     nameInput.value = profileName.textContent;
+//   jobInput.value = profileInfo.textContent;
+// })
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
@@ -57,38 +69,93 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-];
+]
 const popupElementAdd = document.querySelector('.popup_add')
 const popupCreate = popupElement.querySelector('.popup__create');
 const popupAdd = document.querySelector('.profile__add');
-let inputName = document.querySelector('.popup__text_add_name');
-let inputLink = document.querySelector('.popup__text_add_link');
-let elementTitle = document.querySelector('.element__text');
-let elementLink = document.querySelector('.element__mask-group');
-let popupAddClose = document.querySelector('.popup__close_add');
-let formElementAdd = document.querySelector('.popup__content_add');
-const templateElement = document.querySelector('#addtemplate');
+const inputName = document.querySelector('.popup__text_add_name');
+const inputLink = document.querySelector('.popup__text_add_link');
+const popupAddClose = popupElementAdd.querySelector('.popup__close');
+const formElementAdd = document.querySelector('.popup__content_add');
+const templateElement = document.querySelector('#addtemplate').content.querySelector('.element');
+const elements = document.querySelector('.elements');
+
+
+function createElement(item) {
+  const template = templateElement.cloneNode(true);
+  const likeButton = template.querySelector('.element__like');
+  const deleteButton = template.querySelector('.element__delete');
+  const elementTitle = template.querySelector('.element__text');
+  likeButton.addEventListener('click', likeButtonClick);
+  deleteButton.addEventListener('click', deleteButtonClick);
+  elementTitle.textContent = item.name;
+  const elementLink = template.querySelector('.element__mask-group');
+  elementLink.src = item.link;
+  return template;
+
+}
+
+const likeButtonClick = (e) => {
+  e.target.classList.toggle('element__like_active')
+}
+
+const deleteButtonClick = (e) => {
+  e.target.closest('.element').remove()
+}
+
+
+
+const render = (item, wrapElement) => {
+  const element = createElement(item);
+  wrapElement.append(element);
+}
+
+initialCards.forEach(function (item) {
+  render(item, elements)
+})
+
+
+const handleFormSubmit = (e) => {
+  e.preventDefault()
+  const addElement = [{
+    name: inputName.value,
+    link: inputLink.value
+  }];
+  render(addElement, elements);
+  closePopup();
+  e.target.reset();
+}
 
 function openPopupAdd() {
-  popupElementAdd.classList.add('.popup_add_is-opened');
- 
-}
+  popupElementAdd.classList.add('popup_is-opened');
 
+}
 function closePopupAdd() {
-  popupElementAdd.classList.remove('.popup_add_is-opened');
-}
-function formSubmitHandlerAdd(evt) {
-  evt.preventDefault();
-
-  // elementTitle.textContent = inputName.value;
-  // elementLink.textContent = inputLink.value;
-  closePopupAdd();
+  popupElementAdd.classList.remove('popup_is-opened');
 }
 
-formElementAdd.addEventListener('submit', formSubmitHandlerAdd);
+formElementAdd.addEventListener('submit', formSubmitHandler);
 popupAdd.addEventListener('click', openPopupAdd);
 popupAddClose.addEventListener('click', closePopupAdd);
+formElementAdd.addEventListener('submit', handleFormSubmit)
 
-// initialCards.forEach(function(item){
-//   console.log(item);
-// })
+
+const popupImg = document.querySelector('.popup__img');
+const popupCloseImg = popupImg.querySelector('.popup__close');
+
+function openPopupImg() {
+  popupImg.classList.add('popup_is-opened');
+}
+function closePopupImg() {
+  popupImg.classList.remove('popup_is-opened');
+}
+
+function formSubmitHandlerImg(evt) {
+  evt.preventDefault();
+  closePopup();
+}
+elementLink.addEventListener('click', openPopupImg);
+popupCloseImg.addEventListener('click', closePopupImg);
+popupImg.addEventListener('submit', formSubmitHandlerImg);
+
+
